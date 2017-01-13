082d7f106a301588ee2b5f5849a93a98429dedc2
Tom Lous <tomlous@gmail.com>
2017-01-13 16:58:40 +0100

    fix?

--- setup/kaggle-download.sh
+++ setup/kaggle-download.sh
@@ -41,19 +41,21 @@ echo "Creating classes"
 ls train/*.jpg | awk '{print gensub(/^.*\/([^\.]*)\..*$/,"\\1","g",$1)}' | uniq
 
 
-for prefix in `ls train/*.jpg | awk '{print gensub(/^.*\/([^\.]*)\..*$/,"\\1","g",$1)}' | uniq` ; do
-    DIR=${prefix}s
-    echo "Working in $DIR ($prefix)"
-    mkdir train/$DIR sample/train/$DIR
-    mkdir valid/$DIR sample/valid/$DIR
-    echo "mv train/$prefix.*.jpg train/$DIR"
-    mv "train/$prefix."*".jpg" "train/$DIR"
-    TOTAL=`ls train/$DIR | sort -R | wc -l`
-    TOPd=`echo "$TOTAL * $CV_FACTOR" | bc`
-    TOP=`printf '%.0f' $TOPd`
-    ls train/$DIR | sort -R | head -n $TOP | xargs -I@ sh -c "mv train/$DIR/@ valid/$DIR/@"
-    ls train/$DIR | sort -R | head -n $SAMPLE_SIZE | xargs -I@ sh -c "cp train/$DIR/@ sample/train/$DIR/@"
-    ls valid/$DIR | sort -R | head -n $CV_SAMPLE_SIZE | xargs -I@ sh -c "cp valid/$DIR/@ sample/valid/$DIR/@"
+for prefix in `ls train/*.jpg | awk '{print gensub(/^.*\/([^\.]*)\..*$/,"\\1","g",$1)}' | uniq` ; do	
+	if [ ! -z "$prefix" ]; then
+	    DIR=${prefix}s
+	    echo "Working in $DIR ($prefix)"
+	    mkdir train/$DIR sample/train/$DIR
+	    mkdir valid/$DIR sample/valid/$DIR
+	    echo "mv train/$prefix.*.jpg train/$DIR"
+	    mv "train/$prefix."*".jpg" "train/$DIR"
+	    TOTAL=`ls train/$DIR | sort -R | wc -l`
+	    TOPd=`echo "$TOTAL * $CV_FACTOR" | bc`
+	    TOP=`printf '%.0f' $TOPd`
+	    ls train/$DIR | sort -R | head -n $TOP | xargs -I@ sh -c "mv train/$DIR/@ valid/$DIR/@"
+	    ls train/$DIR | sort -R | head -n $SAMPLE_SIZE | xargs -I@ sh -c "cp train/$DIR/@ sample/train/$DIR/@"
+	    ls valid/$DIR | sort -R | head -n $CV_SAMPLE_SIZE | xargs -I@ sh -c "cp valid/$DIR/@ sample/valid/$DIR/@"
+	fi
 done
 
 

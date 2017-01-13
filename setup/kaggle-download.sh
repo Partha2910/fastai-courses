2a69d3e8e8244eaef51ad2324461611868fdc5e2
Tom Lous <tomlous@gmail.com>
2017-01-13 16:26:15 +0100

    space tab

--- setup/kaggle-download.sh
+++ setup/kaggle-download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $# -eq 0 ]] ; then
-    echo '[path] [[CV_FACTOR]] [[SAMPLE_SIZE]]'
-    exit 1
+	echo '[path] [[CV_FACTOR]] [[SAMPLE_SIZE]]'
+	exit 1
 fi
 
 PATH=$1
@@ -28,16 +28,16 @@ mkdir -p sample/train
 mkdir -p sample/valid
 
 for prefix in `ls train/*.jpg | awk '{print gensub(/^.*\/([^\.]*)\..*$/,"\\1","g",$1)}' | uniq` ; do
-    DIR=${prefix}s
-    mkdir train/$DIR sample/train/$DIR
-    mkdir valid/$DIR sample/valid/$DIR
-    mv "train/$prefix."*".jpg" "train/$DIR"
-    TOTAL=`ls train/$DIR | sort -R | wc -l`
-    TOPd=`echo "$TOTAL * $CV_FACTOR" | bc`
-    TOP=`printf '%.0f' $TOPd`
-    ls train/$DIR | sort -R | head -n $TOP | xargs -I@ sh -c "mv train/$DIR/@ valid/$DIR/@"
-    ls train/$DIR | sort -R | head -n $SAMPLE_SIZE | xargs -I@ sh -c "cp train/$DIR/@ sample/train/$DIR/@"
-    ls valid/$DIR | sort -R | head -n $CV_SAMPLE_SIZE | xargs -I@ sh -c "cp valid/$DIR/@ sample/valid/$DIR/@"
+	DIR=${prefix}s
+	mkdir train/$DIR sample/train/$DIR
+	mkdir valid/$DIR sample/valid/$DIR
+	mv "train/$prefix."*".jpg" "train/$DIR"
+	TOTAL=`ls train/$DIR | sort -R | wc -l`
+	TOPd=`echo "$TOTAL * $CV_FACTOR" | bc`
+	TOP=`printf '%.0f' $TOPd`
+	ls train/$DIR | sort -R | head -n $TOP | xargs -I@ sh -c "mv train/$DIR/@ valid/$DIR/@"
+	ls train/$DIR | sort -R | head -n $SAMPLE_SIZE | xargs -I@ sh -c "cp train/$DIR/@ sample/train/$DIR/@"
+	ls valid/$DIR | sort -R | head -n $CV_SAMPLE_SIZE | xargs -I@ sh -c "cp valid/$DIR/@ sample/valid/$DIR/@"
 done
 
 
@@ -47,11 +47,11 @@ done
 #ls train/*.jpg | awk '{gsub(/\..*$/,"", $1);print}' | uniq | xargs -I"{}" sh -c 'mv {}.*.jpg {}s'
 
 #for d in train/* ; do
-#    TOTAL=`ls $d | sort -R | wc -l`
-#    TOPd=`echo "$TOTAL * $SAMPLE_FACTOR" | bc`
-#    TOP=`printf '%.0f' $TOPd`
-#    echo $TOP
-#    ls $d | sort -R | head -n $TOP | xargs
+#	TOTAL=`ls $d | sort -R | wc -l`
+#	TOPd=`echo "$TOTAL * $SAMPLE_FACTOR" | bc`
+#	TOP=`printf '%.0f' $TOPd`
+#	echo $TOP
+#	ls $d | sort -R | head -n $TOP | xargs
 #done
 
 #rm test.zip
